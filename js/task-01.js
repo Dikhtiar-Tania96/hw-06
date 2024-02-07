57845398e1f9a66c6574f3a278120d4cfaded1ee
Dikhtiar-Tania96 <chygynovatania96@gmail.com>
2024-02-07 10:52:44 +0200

    add two new file

--- js/task-01.js
+++ js/task-01.js
@@ -1,13 +1,63 @@
-const categoriesEl = document.querySelector('#categories');
-console.log(categoriesEl);
+// const categoriesEl = document.querySelector('#categories');
+// console.log(categoriesEl);
 
-const list = [...categoriesEl.children];
-console.log(list);
+// const listEl = [...categoriesEl.children];
+// console.log(listEl);
 
-const numberList = categoriesEl.children.length;
-console.log(`Number of categories: `, numberList);
+// const numberList = categoriesEl.children.length;
+// console.log(`Number of categories: `,numberList);
 
-list.forEach((item) => {
-    console.log(`Category: ${item.firstElementChild.textContent}`);
-    console.log(`Elements: ${item.lastElementChild.children.length}`)
-})
+// listEl.forEach((item) => {
+//     console.log(`Category: ${item.firstElementChild.textContent}`);
+//     console.log(`Elements: ${item.lastElementChild.children.length}`)
+// }
+// )
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//const categoriesEl = document.querySelector('#categories');
+// console.log(categoriesEl);
+
+// const list = [...categoriesEl.children];
+// console.log(list);
+
+// const numberList = categoriesEl.children.length;
+// console.log(`Number of categories: `, numberList);
+
+// list.forEach((item) => {
+//     console.log(`Category: ${item.firstElementChild.textContent}`);
+//     console.log(`Elements: ${item.lastElementChild.children.length}`)
+// })
--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,43 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+// const ingredientsEl = document.querySelector('#ingredients');
+// console.log(ingredientsEl);
+
+
+// const makeItems = elements => {
+//   return elements.map(el => {
+//     const newItem = document.createElement('li');
+//     newItem.classList.add('list');
+//     newItem.textContent = el;
+//     return newItem;
+//   })
+// }
+
+// const newArr = makeItems(ingredients);
+// ingredientsEl.append(...newArr)
+
+
+
+
+
+
+
+
+
+
+
+// const makeItems = elements => {
+//   return elements.map(el => {
+//       const itemEl = document.createElement("li");
+//       itemEl.classList.add("item");
+//       itemEl.textContent = el;
+//       console.log(itemEl);
+//       return itemEl;
+//   })
+// };
+
+// const items = makeItems(ingredients);
+// const ulEl = document.querySelector("#ingredients");
+// ulEl.prepend(...items);
