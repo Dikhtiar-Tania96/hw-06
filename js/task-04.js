cac08ecb18ad135eb70fb8c59331d57ffaf695dd
Dikhtiar-Tania96 <chygynovatania96@gmail.com>
2024-02-21 14:18:26 +0200

    add task 4

---
+++ js/task-04.js
@@ -0,0 +1,19 @@
+// const counter = document.querySelector('#counter');
+
+// const btnDecr = document.querySelector('[data-action="decrement"]');
+// const btnIncr = document.querySelector('[data-action="increment"]');
+// const valueEl = document.querySelector('#value');
+
+// btnDecr.addEventListener('click', decrement);
+// btnIncr.addEventListener('click', increment);
+// let counterValue = 0;
+
+// function decrement(){
+// counterValue -= 1;
+// valueEl.textContent = counterValue
+// }
+
+// function increment(){
+// counterValue += 1;
+// valueEl.textContent = counterValue
+// }
