9b7ffdb2477aee19a5a7ba6b07b84235b3e027df
Dikhtiar-Tania96 <chygynovatania96@gmail.com>
2024-02-26 15:31:53 +0200

    add task5

---
+++ js/task-05.js
@@ -0,0 +1,54 @@
+const inputName = document.querySelector('#name-input');
+const outputName = document.querySelector('#name-output');
+
+inputName.addEventListener('input', changeName);
+
+function changeName(evt){
+    let userName = evt.currentTarget.value.trim();
+
+    if(!userName){
+        outputName.textContent = 'Anonymous'
+    } else {
+        outputName.textContent = userName;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const inputName = document.querySelector('#name-input');
+// const outputName = document.querySelector('#name-output');
+
+// inputName.addEventListener('input', changeName);
+// function changeName(evt) {
+//    let userName = evt.currentTarget.value.trim();  
+//     if (!userName) {
+//            outputName.textContent = "Anonymous"
+//     } else {
+//         outputName.textContent = userName;
+//        }
+// }
