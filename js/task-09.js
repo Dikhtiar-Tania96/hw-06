3f34ddecad82f6271ef23d379ad5fd69ab4af202
Dikhtiar-Tania96 <chygynovatania96@gmail.com>
2024-02-28 14:48:11 +0200

    add task 9

--- js/task-09.js
+++ js/task-09.js
@@ -1,5 +1,35 @@
+const body = document.querySelector('body');
+const spanText = document.querySelector('.color');
+const button = document.querySelector('.change-color');
+
+button.addEventListener('click', onClick);
+
+function onClick(){
+  body.style.backgroundColor = getRandomHexColor();
+  spanText.textContent = (getRandomHexColor())
+}
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+
+
+// const button = document.querySelector('.change-color');
+// const body = document.querySelector('body');
+// const textEl = document.querySelector('.color');
+
+// button.addEventListener("click", onClick);
+
+// function onClick() {
+//   body.style.backgroundColor = getRandomHexColor();
+//   textEl.textContent = (getRandomHexColor())
+// }
+
+// function getRandomHexColor() {
+//   return `#${Math.floor(Math.random() * 16777215)
+//     .toString(16)
+//     .padStart(6, 0)}`;
+// }
