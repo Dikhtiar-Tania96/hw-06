d461277addbad7b7ff4c5aca261e71548e343364
Dikhtiar-Tania96 <chygynovatania96@gmail.com>
2024-02-28 14:34:35 +0200

    add task 8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,45 @@
+const formEl = document.querySelector('.login-form');
+formEl.addEventListener('submit', inSubmit);
 
+function inSubmit(evt){
+    evt.preventDefault();
+    const { email, password } = evt.currentTarget.elements;
+
+
+    if(email.value === '' || password.value === ''){
+        return alert('Всі поля повинні бути заповнені!')
+    } 
+
+    const data = {
+        email : email.value,
+        password : password.value
+    };
+    console.log(data);
+
+    evt.currentTarget.reset()
+}
+
+
+
+
+
+
+// const formEl = document.querySelector('.login-form');
+// console.dir
+// formEl.addEventListener('submit', onSubmit);
+
+// function onSubmit(evt) {
+//     evt.preventDefault();
+//     const { email, password } = evt.currentTarget.elements;
+   
+//     if (!email.value || !password.value) {
+//        return alert("Fields could not be empty!")
+//     } 
+
+//     const data = {
+//         email: email.value,
+//         password: password.value,
+//     }
+//     console.log(data);
+//     evt.currentTarget.reset();
+// }
